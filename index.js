0e8141a4158af48c47d8fd4301f13f5da6cc1451
qianphong <qianphong@163.com>
2021-11-16 16:47:42 +0800

    feat: init

---
+++ index.js
@@ -0,0 +1,8 @@
+module.exports = {
+  extends: '@antfu',
+  rules: {
+    'no-console': 'off',
+    'vue/singleline-html-element-content-newline': 'off',
+    'brace-style': ['error', '1tbs'],
+  },
+}
